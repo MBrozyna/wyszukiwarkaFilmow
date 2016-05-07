33e9bdfa3f3f5cf55a85b7be624792079ba861df
Mateusz <matbrozyna7@gmail.com>
2016-05-07 18:29:28 +0200

    update widoku admina

--- modules/films/client/controllers/films.client.controller.js
+++ modules/films/client/controllers/films.client.controller.js
@@ -5,19 +5,59 @@
   angular
     .module('films')
     .controller('FilmsController', FilmsController);
+    /*.filter('TypeFilter', TypeFilter);*/
 
-  FilmsController.$inject = ['$scope', '$state', 'Authentication', 'filmResolve', 'FilmsService'];
+  FilmsController.$inject = ['$scope', '$state', '$q', 'Authentication', 'filmResolve', 'FilmsService'];
 
-  function FilmsController ($scope, $state, Authentication, film, FilmsService) {
+  function FilmsController ($scope, $state, $q, Authentication, film, FilmsService) {
     var vm = this;
 
     vm.authentication = Authentication;
-    vm.film = film;
-    vm.films = FilmsService.query();
+    vm.films = [];
+    vm.types = [];
+    vm.cast = [];
+    vm.countries = [];
+    doQuery().then(function(films){
+      var arrTypes = [];
+      var arrCountry= [];
+      var arrActor = [];
+      for(var i =0; i<films.length;i++){
+        for(var j=0;j<films[i].type.length;j++){
+          if (arrTypes.indexOf(films[i].type[j]) === -1) {
+            arrTypes.push(films[i].type[j]);
+          }
+        }
+        for(j=0;j<films[i].country.length;j++){
+          if (arrCountry.indexOf(films[i].country[j]) === -1) {
+            arrCountry.push(films[i].country[j]);
+          }
+        }
+        for(j=0;j<films[i].cast.length;j++){
+          if (arrActor.indexOf(films[i].cast[j]) === -1) {
+            arrActor.push(films[i].cast[j]);
+          }
+        }
+      }
+      vm.films = films;
+      vm.countries = arrCountry;
+      vm.cast = arrActor;
+      vm.types = arrTypes;
+    },
+    function(){
+      console.log('error');
+    });
     vm.error = null;
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
+
+    function doQuery(){
+      var d = $q.defer();
+      var result = FilmsService.query({},function(){
+        d.resolve(result);
+      });
+      return d.promise;
+    }
 
     // Remove existing Film
     function remove() {
@@ -50,8 +90,5 @@
         vm.error = res.data.message;
       }
     }
-    $scope.types = function(films) {
-      return user.User.Stats[0].active === "1";
-    };
   }
 })();
